2d9b6348a0b8886d0217ed68d7aedb62f2a4d089
ehorseracing <mike@ehorseracing.com>
2023-11-09 13:51:16 -0600

    Testing spin-wheel mods
    Testing spin-wheel mods

--- script.js
+++ script.js
@@ -7,32 +7,23 @@ const spinValues = [
   { minDegree: 61, maxDegree: 90, value: 100 },
   { minDegree: 31, maxDegree: 60, value: 200 },
   { minDegree: 0, maxDegree: 30, value: 300 },
-  { minDegree: 331, maxDegree: 360, value: 400 },
-  { minDegree: 301, maxDegree: 330, value: 500 },
-  { minDegree: 271, maxDegree: 300, value: 600 },
-  { minDegree: 241, maxDegree: 270, value: 700 },
-  { minDegree: 211, maxDegree: 240, value: 800 },
-  { minDegree: 181, maxDegree: 210, value: 900 },
-  { minDegree: 151, maxDegree: 180, value: 1000 },
-  { minDegree: 121, maxDegree: 150, value: 1100 },
-  { minDegree: 91, maxDegree: 120, value: 1200 },
+  { minDegree: 301, maxDegree: 360, value: 400 },
+  { minDegree: 241, maxDegree: 300, value: 500 },
+  { minDegree: 91, maxDegree: 240, value: 600 },
+
 ];
 /* --------------- Size Of Each Piece  --------------------- */
-const size = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+const size = [10, 10, 10, 20, 20, 50];
 /* --------------- Background Colors  --------------------- */
 var spinColors = [
   "#E74C3C",
-  "#7D3C98",
   "#2E86C1",
   "#138D75",
   "#F1C40F",
   "#D35400",
   "#138D75",
-  "#F1C40F",
-  "#b163da",
-  "#E74C3C",
-  "#7D3C98",
-  "#138D75",
+
+
 ];
 /* --------------- Chart --------------------- */
 /* --------------- Guide : https://chartjs-plugin-datalabels.netlify.app/guide/getting-started.html --------------------- */
@@ -40,7 +31,7 @@ let spinChart = new Chart(spinWheel, {
   plugins: [ChartDataLabels],
   type: "pie",
   data: {
-    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    labels: [1, 2, 3, 4, 5, 6, ],
     datasets: [
       {
         backgroundColor: spinColors,
